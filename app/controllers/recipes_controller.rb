8e204d14e131cdb59b159e1af9e4be1c62f89942
cy porteous <cy.porteous@pm.me>
2024-03-08 14:39:19 +0000

    cloudinary with add photo feature along with new recipe creation

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -17,7 +17,7 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
-    #@recipe.recipe_ingredients.build
+    @recipe.recipe_ingredients.build
     # 10.times {@recipe.recipe_ingredients.build}
   end
 
