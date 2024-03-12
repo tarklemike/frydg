3f6c5612119b854dd5df85de8540ce52250704f3
Grace Macintosh <grace.macintosh@icloud.com>
2024-03-12 18:11:39 +0000

    allergen filters all working

--- app/javascript/controllers/filtered_index_controller.js
+++ app/javascript/controllers/filtered_index_controller.js
@@ -2,39 +2,34 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="filtered-index"
 export default class extends Controller {
+  static targets = ["checkbox", 'container']
   connect() {
     console.log("hello");
   }
 
-  // for allergens
-  gluten() {
-    console.log("gluten");
-  }
+  sendRequest(event) {
+    console.log(this.checkboxTargets);
+    let glutenFree = null
+    let dairyF = null
+    let vegetarian = null
+    let vegan = null
+    console.log(glutenFree)
+    this.checkboxTargets.forEach((checkbox)=>{
+      if (checkbox.checked && checkbox.id === "gluten_free") glutenFree = true;
+      if (checkbox.checked && checkbox.id === "dairy_f") dairyF = true;
+      if (checkbox.checked && checkbox.id === "vegetarian") vegetarian = true;
+      if (checkbox.checked && checkbox.id === "vegan") vegan = true;
+      // this.dairyFree = (checkbox.checked && checkbox.id === "dairy_free") ? true : false;
 
-  dairy() {
-    console.log("dairy");
-  }
+    })
+    console.log(glutenFree);
+    const url = `/recipes/?query=true&gluten_free=${glutenFree}&dairy_f=${dairyF}&vegetarian=${vegetarian}&vegan=${vegan}`
+    localStorage.setItem("filteredUrl", url)
+    fetch(url, {headers: {'Accept': 'text/plain'}})
+    .then(response => response.text())
+    .then((data) => {
+      this.containerTarget.innerHTML = data
+    })
 
-  vegitarian() {
-    console.log("vegitarian");
-  }
-
-  vegan() {
-    console.log("vegan");
-  }
-
-  // For cuisine
-
-  // For difficulty
-  easy() {
-    console.log("easy");
-  }
-
-  medium() {
-    console.log("medium");
-  }
-
-  hard() {
-    console.log("hard");
   }
 }
