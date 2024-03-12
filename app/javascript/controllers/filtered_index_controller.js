eef98ecf0d6f70da7cadc4e946efca51774c6ed2
Grace Macintosh <grace.macintosh@icloud.com>
2024-03-12 15:50:01 +0000

    styling index and filter controller

--- app/javascript/controllers/filtered_index_controller.js
+++ app/javascript/controllers/filtered_index_controller.js
@@ -5,4 +5,36 @@ export default class extends Controller {
   connect() {
     console.log("hello");
   }
+
+  // for allergens
+  gluten() {
+    console.log("gluten");
+  }
+
+  dairy() {
+    console.log("dairy");
+  }
+
+  vegitarian() {
+    console.log("vegitarian");
+  }
+
+  vegan() {
+    console.log("vegan");
+  }
+
+  // For cuisine
+
+  // For difficulty
+  easy() {
+    console.log("easy");
+  }
+
+  medium() {
+    console.log("medium");
+  }
+
+  hard() {
+    console.log("hard");
+  }
 }
