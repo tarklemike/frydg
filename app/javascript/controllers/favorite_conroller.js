fe357e7e5ba8bdafa2eda635750bae60c32b56b6
torentino evans <Torentinoevans@gmail.com>
2024-03-13 17:38:27 +0000

    when a review is left on a recipe it doesn't scroll to the top of the page anymore

---
+++ app/javascript/controllers/favorite_conroller.js
@@ -0,0 +1,3 @@
+const button = document.querySelector("#fa-regular fa-heart")
+button.addEventListener("click", (event) => {
+  console.log(event);
---
+++ app/javascript/controllers/insert_reviews_controller.js
@@ -0,0 +1,27 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="insert-reviews"
+export default class extends Controller {
+  static targets =['form', 'list']
+
+  connect() {
+    console.log("Hello from insert reviews")
+    console.log(this.formTarget)
+  }
+
+  insert(e) {
+    e.preventDefault()
+    fetch(this.formTarget.action, {
+      method: 'POST',
+      headers: {"Accept": "application/json"},
+      body: new FormData(this.formTarget)
+    })
+    .then(res => res.json())
+    .then((data) => {
+      if (data.inserted_item){
+        this.listTarget.insertAdjacentHTML('beforeend', data.inserted_item)
+      }
+      this.formTarget.outerHTML = data.form
+    })
+  }
+}
