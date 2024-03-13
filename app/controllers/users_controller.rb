43c1fd4abbf045477bf963ef059cf19884df9c45
Romulo Arias <raariasalvarez@gmail.com>
2024-03-13 16:51:32 +0000

    routes for profile nav sorted

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:edit, :update]
+  before_action :set_user, only: [:edit, :update, :show]
   before_action :correct_user, only: [:edit, :update]
 
   def show
